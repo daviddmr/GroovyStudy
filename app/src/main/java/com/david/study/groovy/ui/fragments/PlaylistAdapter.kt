70e00abf6557dd5833717c6749be4f2beff78613
David Ribeiro <david.ribeiro@bookjane.com>
2023-02-07 22:17:45 -0500

    Create PlaylistsAdapter with auto generating tool

---
+++ app/src/main/java/com/david/study/groovy/ui/fragments/PlaylistAdapter.kt
@@ -0,0 +1,34 @@
+package com.david.study.groovy.ui.fragments
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.david.study.groovy.databinding.ItemPlaylistsBinding
+import com.david.study.groovy.model.Playlist
+
+class PlaylistAdapter(
+    private val values: List<Playlist>
+) : RecyclerView.Adapter<PlaylistAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemPlaylistsBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+//        holder.playlistImage.text = item.id
+        holder.playlistTitle.text = "Title 01"
+        holder.playlistCategory.text = "Category 01"
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: ItemPlaylistsBinding) : RecyclerView.ViewHolder(binding.root) {
+        val playlistImage: ImageView = binding.itemPlaylistsImageIv
+        val playlistTitle: TextView = binding.itemPlaylistsPlaylistNameTv
+        val playlistCategory: TextView = binding.itemPlaylistsPlaylistCategoryTv
+    }
+
+}
