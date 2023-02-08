cbf71fd4d129986c0405ccb5e5b8a3562570dd74
David Ribeiro <david.ribeiro@bookjane.com>
2023-02-08 15:58:30 -0500

    Temporarily remove implementation of PlaylistRepository

--- app/src/main/java/com/david/study/groovy/repository/PlaylistRepository.kt
+++ app/src/main/java/com/david/study/groovy/repository/PlaylistRepository.kt
@@ -4,16 +4,17 @@ import com.david.study.groovy.model.Playlist
 
 class PlaylistRepository {
     suspend fun getPlaylists(): Result<List<Playlist>> {
-        val playlists = mutableListOf<Playlist>()
-        for (i in 0..10) {
-            val playlist = Playlist("$i", "Playlist $i", "Category $i")
-            playlists.add(playlist)
-        }
-
-        return if (playlists.isNotEmpty()) {
-            Result.success(playlists)
-        } else {
-            Result.failure(java.lang.RuntimeException("No result"))
-        }
+//        val playlists = mutableListOf<Playlist>()
+//        for (i in 0..10) {
+//            val playlist = Playlist("$i", "Playlist $i", "Category $i")
+//            playlists.add(playlist)
+//        }
+//
+//        return if (playlists.isNotEmpty()) {
+//            Result.success(playlists)
+//        } else {
+//            Result.failure(java.lang.RuntimeException("No result"))
+//        }
+        TODO("Not implemented yet")
     }
 }
