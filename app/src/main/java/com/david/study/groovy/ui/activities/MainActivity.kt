15899bbad084a73da746ed8ec8f70a2602c2159d
David Ribeiro <david.ribeiro@bookjane.com>
2023-02-07 22:37:34 -0500

    Open Playlists fragment on MainActivity

--- app/src/main/java/com/david/study/groovy/ui/activities/MainActivity.kt
+++ app/src/main/java/com/david/study/groovy/ui/activities/MainActivity.kt
@@ -3,10 +3,15 @@ package com.david.study.groovy.ui.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.david.study.groovy.R
+import com.david.study.groovy.ui.fragments.PlaylistsFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.main_activity_container, PlaylistsFragment.newInstance())
+            .commit()
     }
 }
