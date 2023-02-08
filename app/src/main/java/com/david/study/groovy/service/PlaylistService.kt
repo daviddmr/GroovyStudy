1108fead04c2260f42051ddb9cf72413aa06a2ed
David Ribeiro <david.ribeiro@bookjane.com>
2023-02-08 17:22:23 -0500

    Change return of fetchPlaylists on PlaylistService

--- app/src/main/java/com/david/study/groovy/service/PlaylistService.kt
+++ app/src/main/java/com/david/study/groovy/service/PlaylistService.kt
@@ -1,7 +1,9 @@
 package com.david.study.groovy.service
 
+import com.david.study.groovy.model.Playlist
+
 class PlaylistService {
-    fun fetchPlaylists() {
+    fun fetchPlaylists(): Result<List<Playlist>> {
         TODO("Not yet implemented")
     }
 }
