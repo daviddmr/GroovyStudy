ab0a56e4fbd482f21e0314c3bfe24d2592929951
David Ribeiro <david.ribeiro@bookjane.com>
2023-02-08 20:36:43 -0500

    Write test to check if playlistApi.fetchAllPlaylists() is called from PlaylistService.fetchPlaylists()

--- app/src/test/java/com/david/study/groovy/service/PlaylistServiceTest.kt
+++ app/src/test/java/com/david/study/groovy/service/PlaylistServiceTest.kt
@@ -1,5 +1,23 @@
 package com.david.study.groovy.service
 
-import org.junit.Assert.*
+import com.david.study.groovy.interfaces.PlaylistApi
+import com.david.study.groovy.utils.BaseUnitTest
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
 
-class PlaylistServiceTest
+class PlaylistServiceTest : BaseUnitTest() {
+
+    private val playlistApi: PlaylistApi = mock()
+
+    @Test
+    fun fetchPlaylistsShouldCallFetchAllPlaylists() {
+        runBlocking {
+            val sut = PlaylistService(playlistApi)
+            sut.fetchPlaylists()
+            verify(playlistApi, times(1)).fetchAllPlaylists()
+        }
+    }
+}
